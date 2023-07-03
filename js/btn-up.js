c9fbcc5bfec6179cf1421ded9c83faf05709518d
iudinant <131783168+iudinant@users.noreply.github.com>
2023-07-03 22:53:01 +0200

    ff

--- js/btn-up.js
+++ js/btn-up.js
@@ -17,3 +17,17 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+const btnToUp = document.querySelector('.btn-up');
+btnToUp.addEventListener('click', () => {
+   window.scrollTo(0, 0);
+});
+
+const btnScroll = document.querySelector('.btn-up');
+window.onscroll = () => {
+   if (window.scrollY > 700) {
+      btnScroll.classList.remove('btn-hidden');
+   } else if (window.scrollY < 700) {
+      btnScroll.classList.add('btn-hidden');
+   }
+}
